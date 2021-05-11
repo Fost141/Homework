c708ed97a3d745fa631dc1b5f90277712162f904
Fost141 <georgrussib@gmail.com>
2021-05-12 02:51:25 +0700

    HomeWork_1

---
+++ HomeWork_1/index.js
@@ -0,0 +1,16 @@
+let g = 'G';
+let e = 'e';
+let o = 'o';
+let r = 'r';
+let gg = 'g';
+let myName = g + e + o + r + gg;
+console.log(myName);
+let p = document.querySelector('p');
+console.log(p);
+
+p.style.display = ('flex');
+p.style.justifyContent = ('center');
+p.style.fontSize = ('2.3em');
+p.style.color = ('darkslateblue');
+
+document.getElementById("new__text").innerHTML = "Изменение текста параграфа при помощи JS";
